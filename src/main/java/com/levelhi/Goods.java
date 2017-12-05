1c4b90bd38da9185023382b53e2d0a3d0b377a8c
levelhi412 <levelhi@sina.com>
2017-12-05 17:06:05 +0800

    first
    我的hibernate练习，这次提交是环境搭建和添加都OK。

---
+++ src/main/java/com/levelhi/Goods.java
@@ -0,0 +1,80 @@
+package com.levelhi;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @Author:jiexuan
+ * @Description:
+ * @Date: Created in 15:50 2017/12/5
+ * @Modified By:
+ */
+@Entity
+public class Goods {
+    private int sid;
+    private String name;
+    private Double price;
+
+    public Goods() {
+    }
+
+    public Goods(int sid, String name, Double price) {
+        this.sid = sid;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Id
+    @Column(name = "sid")
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "price")
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Goods goods = (Goods) o;
+
+        if (sid != goods.sid) return false;
+        if (name != null ? !name.equals(goods.name) : goods.name != null) return false;
+        if (price != null ? !price.equals(goods.price) : goods.price != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = sid;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/test/java/com/levelhi/TestGoods.java
@@ -0,0 +1,59 @@
+package com.levelhi;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * @Author:jiexuan
+ * @Description:
+ * @Date: Created in 15:13 2017/12/5
+ * @Modified By:
+ *
+ * 新学的快捷键：自动补全变量名称 : Ctrl + Alt + v
+                自动补全属性名称 : Ctrl + Alt + f
+ *
+ * 参考文档：http://blog.csdn.net/xubaifu1997/article/details/66476446
+ */
+public class TestGoods {
+
+    private Configuration configuration;
+    private SessionFactory sessionFactory;
+    private Session currentSession;
+    private Transaction transaction;
+
+    @Before
+    public void init(){
+        configuration = new Configuration().configure();            //创建配置对象
+        sessionFactory = configuration.buildSessionFactory();       //创建会话工厂
+        currentSession = sessionFactory.getCurrentSession();        //开启会话
+        transaction = currentSession.beginTransaction();            //开启事务
+    }
+
+    /**
+     *  这里注意因为我用的是getCurrentSession，所以自动关闭session，
+     *  这里如果用这个注意还要在hibernate.cfg.xml里面进行
+     *  相应配置，而如果用openSession就要主动手动关闭session了。
+     *
+     */
+    @After
+    public void destroy(){
+        transaction.commit();       //提交事务
+        sessionFactory.close();     //关闭会话工厂
+    }
+
+    @Test
+    public void testGoods(){
+        //生成对象
+        Goods goods = new Goods(2,"女朋友",27.7);
+        //保存对象进数据库
+        currentSession.save(goods);
+
+    }
+
+}
